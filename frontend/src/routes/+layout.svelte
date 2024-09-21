166930c99216870ae9e52062677098ff9f3d3272
Darryl Yeo <dev@darryl-yeo.com>
2024-09-20 23:01:32 -0500

    Layout: initialize with AppKit

---
+++ frontend/src/routes/+layout.svelte
@@ -0,0 +1,66 @@
+<script lang="ts">
+	// Context
+	import { browser } from '$app/environment'
+
+
+	// Inputs
+	let {
+		children,
+	} = $props()
+
+
+	// Internal state
+	import type { AppKit } from '@reown/appkit'
+
+	let appKit: AppKit | undefined = $state()
+
+	$effect(() => {
+		if(browser) (async () => {
+			const { arbitrum, mainnet } = await import('@reown/appkit/networks')
+			const { WagmiAdapter } = await import('@reown/appkit-adapter-wagmi')
+			const { createAppKit } = await import('@reown/appkit')
+
+			appKit = createAppKit({
+				adapters: [
+					new WagmiAdapter({
+						networks: [mainnet, arbitrum],
+						projectId: import.meta.env.VITE_PROJECT_ID,
+					})
+				],
+				metadata: {
+					name: 'Html Example',
+					description: 'Html Example',
+					url: 'https://reown.com/appkit',
+					icons: ['https://avatars.githubusercontent.com/u/37784886'],
+				},
+				networks: [mainnet, arbitrum],
+				projectId: import.meta.env.VITE_PROJECT_ID,
+				themeMode: 'light',
+			})
+		})()
+	})
+
+
+	// Styles
+	import '../global.css'
+</script>
+
+
+<header class="row">
+	<h1>App</h1>
+
+	<div class="row">
+		<w3m-button></w3m-button>
+		<w3m-network-button></w3m-network-button>
+	</div>
+</header>
+
+
+{@render children()}
+
+
+<style>
+	header {
+		padding: 1rem;
+	}
+</style>
