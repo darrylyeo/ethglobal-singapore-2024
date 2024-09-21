92fc4a07d4490ac58a9001177a68181fc58d7372
eldante <dan.tgw@gmail.com>
2024-09-22 05:25:26 +0800

    Added matchmaking smart contract.

--- contracts/contracts/MatchMaking.sol
+++ contracts/contracts/MatchMaking.sol
@@ -1,30 +1,25 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.0;
 
-contract MatchMaking {
-    mapping(address => int8[]) private _usersResponse;
+contract Matchmaking {
+    mapping(address => uint8[]) private _usersResponse;
 
-    function setUserResponse(int8[] memory response) external {
+    function setUserResponse(uint8[] memory response) external {
         _usersResponse[msg.sender] = response;
     }
 
-    function compatibilityScore(address stranger) external view returns (int8, int8, int8) {
-      // if (msg.sender!=author) {
-      //   revert("not allowed");
-      // }
-      int8[] memory senderResponse = _usersResponse[msg.sender];
-      int8[] memory partnerResponse = _usersResponse[stranger];
-      // For loop to compare the responses
-      int8 score = 0;
-      int8 denominatorA = 0;
-      int8 denominatorB = 0;
+    function compatibilityScore(address stranger) external view returns (uint8) {
+      uint8[] memory senderResponse = _usersResponse[msg.sender];
+      uint8[] memory partnerResponse = _usersResponse[stranger];
+      uint8 score = 0;
 
       for (uint i = 0; i < senderResponse.length; i++) {
-        score += senderResponse[i] * partnerResponse[i];
-        denominatorA += senderResponse[i] * senderResponse[i];
-        denominatorB += partnerResponse[i] * partnerResponse[i];
+        if(senderResponse[i] == partnerResponse[i]){
+          score += 5;
+        }
       }
 
-      return (score, denominatorA, denominatorB);
+      return score;
     }
 }
+
